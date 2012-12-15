7365bc53789b6c92550be6492732058a15ae86d9
Carl Boettiger <cboettig@gmail.com>
2012-12-14 17:53:27 -0800

    avoid NaNs in F calculation (too agressive?)

--- R/gp_trasition_matrix.R
+++ R/gp_trasition_matrix.R
@@ -76,21 +76,30 @@ f_transition_matrix <- function(f, p, x_grid, h_grid = NULL, sigma_g){
 #' @export
 par_F <- function(h, f, p, x_grid, sigma_g){
   mu <- sapply(x_grid, f, h, p)
-  F_true <- t(sapply(mu, function(m){
-    if(m>0) {
-      out <- dlnorm(x_grid/m, 0, sigma_g)
-      
-    } else {
+  F_true <- t(sapply(mu, function(m){    
+    if(snap_to_grid(m,x_grid) < x_grid[2]){ # 
       out <- numeric(length(x_grid))
       out[1] <- 1
       out
+    } else {
+      out <- dlnorm(x_grid/m, 0, sigma_g)
     }
   }))
   F_true <- rownorm(F_true)
 }
   
                        
-                       
+snap_to_grid <- function(x, grid) sapply(x, function(x) grid[which.min(abs(grid - x))])   
+
                        
 # internal helper function
-rownorm <- function(M) t(apply(M, 1, function(x) x/sum(x)))
+rownorm <- function(M) 
+  t(apply(M, 1, function(x){ 
+    if(sum(x)>0){
+      x/sum(x)
+    } else {
+      out <- numeric(length(x))
+      out[1] <- 1
+      out
+    }
+  }))
