69356114b256e93c18e43db8c5e27b951dd84339
Carl Boettiger <cboettig@gmail.com>
2014-05-14 13:01:11 -0700

    script to mangle names and move to dropbox

---
+++ manuscripts/igiveup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp manuscript.pdf ~/Dropbox/GPDP/manuscript-CB-$(date +"%m-%d-%Y").pdf
+cp manuscript.tex ~/Dropbox/GPDP/manuscript-CB-$(date +"%m-%d-%Y").tex
+cp -r components/figures ~/Dropbox/GPDP/components/ 
+cp -r components/elsarticle.cls ~/Dropbox/GPDP/
+
+
