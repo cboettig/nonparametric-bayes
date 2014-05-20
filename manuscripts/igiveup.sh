8e75908f70b8681cf203d00f5e8bf406fc75b226
Carl Boettiger <cboettig@gmail.com>
2014-05-20 16:18:01 -0700

    fix figure dir target

--- manuscripts/igiveup.sh
+++ manuscripts/igiveup.sh
@@ -2,7 +2,7 @@
 
 cp manuscript.pdf ~/Dropbox/GPDP/manuscript-CB-$(date +"%m-%d-%Y").pdf
 cp manuscript.tex ~/Dropbox/GPDP/manuscript-CB-$(date +"%m-%d-%Y").tex
-cp -r components/figures ~/Dropbox/GPDP/components/ 
+cp -r components/figure ~/Dropbox/GPDP/components/ 
 cp -r components/elsarticle.cls ~/Dropbox/GPDP/
 
 
